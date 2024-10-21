dc32412b49dfefb37955e918f83c222caee48c53
Owen Merrill <91266947+TheChozoLord@users.noreply.github.com>
2024-10-21 10:49:10 -0400

    Add files via upload

---
+++ abstractClasses.java
@@ -0,0 +1,32 @@
+package abstractClassesOOP;
+import Java.util.Scanner;
+
+Scanner scan = new Scanner(System.in);
+public class abstractClasses {
+
+    abstract class user{
+        String name;
+        int phoneNumber;
+        String email;
+        String password;
+
+        public nameSet;
+        contactNumber;
+        emailSet;
+        passwordSet;
+    }
+    class customer extends user{
+        int reservation;
+        int partySize;
+
+        reservationTime;
+        partySizeSet;
+    }
+    class worker extends user{
+        String role;
+        String assignedTables;
+
+        roleSet;
+        assignTables;
+    }
+}
---
+++ interfaces.java
@@ -0,0 +1,17 @@
+package abstractClassesOOP;
+
+public class interfaces {
+    public interface{
+        int input;
+        system.out.println("1-Customer 2-Employee");
+        if (input == 1){
+
+        }
+        if (input == 2){
+
+        }
+        else{
+            system.out.println("Please input a valid answer.")
+        }
+    }
+}
